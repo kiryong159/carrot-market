134bd630fbc97a6c8b3bb1da3b4ac582e224eed5
kiryong <116635003+kiryong159@users.noreply.github.com>
2023-06-30 19:49:34 +0900

    5.3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,82 @@
+import tw from "tailwind-styled-components";
+
+const Wrapper = tw.div``;
+
+const Items = tw.div``;
+const ItemInfo = tw.div``;
+const SvgBox = tw.div``;
+const Img = tw.div``;
 export default function Home() {
   return (
-    <div className="m-auto grid w-1/3 gap-10 bg-slate-400 px-10 py-10">
-      <div className="rounded-3xl bg-white p-5 shadow-2xl">
-        <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 flex justify-between">
-          <span className="text-gray-500">Gray Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Tooly Table</span>
-          <span className="font-semibold">$81</span>
-        </div>
-        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
-          <span className="font-semibold">Total</span>
-          <span className="font-semibold">$99</span>
-        </div>
-        <div className="m-auto mt-5 w-1/2 rounded-xl bg-blue-500 px-10 py-3 text-center text-white">
-          CheckOut
-        </div>
-      </div>
-      <div className="rounded-3xl bg-blue-500 py-10 shadow-2xl ">
-        <div className="mb-5 flex justify-between p-6 text-2xl font-semibold text-white">
-          <span>Profile</span>
-          <span>cart</span>
-        </div>
-        <div className="relative top-12 flex h-2/3 flex-col rounded-3xl bg-white p-2 ">
-          <div className="flex justify-around text-center">
-            <div className="flex flex-col">
-              <span className="text-slate-500">Orders</span>
-              <span className="text-lg font-semibold">340</span>
+    <Wrapper className="flex flex-col space-y-5 py-10">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <Items
+          key={i}
+          className="flex cursor-pointer justify-between border-b   px-4 pb-4"
+        >
+          <ItemInfo className="flex space-x-4">
+            <Img className="h-20 w-20 rounded-md bg-gray-500" />
+            <div className="flex flex-col pt-2">
+              <h3 className="text-sm font-bold text-gray-900">New iPhone 14</h3>
+              <span className="text-xs text-gray-500">Black</span>
+              <span className="mt-1 font-bold text-gray-900">$95</span>
             </div>
-            <div className="relative -top-16 h-28 w-28 rounded-full bg-orange-600"></div>
-            <div className="flex flex-col">
-              <span className="text-slate-500">Spent</span>
-              <span className="text-lg font-semibold">$2,310</span>
+          </ItemInfo>
+          <SvgBox className="flex items-end justify-end space-x-2 ">
+            <div className="flex items-center space-x-0.5  text-sm text-gray-600 ">
+              <svg
+                className="h-4 w-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              <span>1</span>
             </div>
-          </div>
-          <div className="-mt-10 flex flex-col text-center">
-            <span className="text-lg font-semibold">Tony Molloy</span>
-            <span className="text-slate-500">New York, USA</span>
-          </div>
-        </div>
-      </div>
-      <div className="rounded-lg bg-white p-10 shadow-2xl"></div>
-      <div className="rounded-lg bg-white p-10 shadow-2xl"></div>
-    </div>
+            <div className="flex items-center space-x-0.5  text-sm text-gray-600 ">
+              <svg
+                className="h-4 w-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+          </SvgBox>
+        </Items>
+      ))}
+      <button className="fixed bottom-24 right-5 rounded-full bg-orange-400 p-4 text-white shadow-xl transition-all hover:bg-orange-600">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
+    </Wrapper>
   );
 }
